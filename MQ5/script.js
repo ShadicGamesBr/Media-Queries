a2024e593e1782088eec3432ba9735eb043a568f
Felipe Kauan <99798470+ShadicGamesBr@users.noreply.github.com>
2023-06-10 14:02:20 -0300

    update v 1.13

---
+++ MQ5/script.js
@@ -0,0 +1,9 @@
+function clicar(){
+    var itens = document.getElementById("abrir")
+    if(itens.style.display == "block"){
+        itens.style.display = "none"
+    }else{
+        itens.style.display = "block"
+    }
+    
+}
