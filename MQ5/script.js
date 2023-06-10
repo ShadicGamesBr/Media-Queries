d16a97368bb12cd7189826960ec02b50907c653c
Felipe Kauan <99798470+ShadicGamesBr@users.noreply.github.com>
2023-06-10 16:46:07 -0300

    update v1.14

--- MQ5/script.js
+++ MQ5/script.js
@@ -1,9 +1,15 @@
+var itens = document.getElementById("abrir")
 function clicar(){
-    var itens = document.getElementById("abrir")
     if(itens.style.display == "block"){
         itens.style.display = "none"
     }else{
         itens.style.display = "block"
     }
     
+} function mudouTamanho(){
+    if(window.innerWidth >= 768){
+        itens.style.display = "block"
+    }else{
+        itens.style.display = "none"
+    }
 }
